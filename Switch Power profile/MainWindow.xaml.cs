893f1993266a9aba991f2987f63078687f147240
KTAppDev <kentaylorappdev@gmail.com>
2020-12-13 22:33:21 -0400

    updates

--- Switch Power profile/MainWindow.xaml.cs
+++ Switch Power profile/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
+using System.Management;
 
 namespace Switch_Power_profile
 {
@@ -23,15 +24,118 @@ namespace Switch_Power_profile
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        static public string LowGUID = "b75672a1-4a7f-4485-9a64-0445bab63964";
+        static public string BalancedGUID = "381b4222-f694-41f0-9685-ff5bb260df2e";
+        static public string HighGUID = "ca85b8be-8830-40df-94f7-86fe770005f1";
+
         public MainWindow()
         {
             
             InitializeComponent();
-            
+            ReadAndUpdateUi();
+
+
+
+            var ts = new ThreadStart(GetChargingStatus);
+            var backgroundThread = new Thread(ts);
+            backgroundThread.Start();
+
+            //MessageBox.Show($"{GetChargingStatus()}\nCurrent Profile = {GetCurrentPowerProfile()}");
+
+
             // scan to see what its on when the prog starts
 
         }
+
+        public void ReadAndUpdateUi()
+        {
+            int State = GetCurrentPowerProfile();
+
+            switch (State)
+            {
+                case 1:
+                    LabelLow.Content = "Selected";
+                    LabelLow.Visibility = Visibility.Visible;
+                    LabelBalanced.Visibility = Visibility.Hidden;
+                    LabelHigh.Visibility = Visibility.Hidden;
+                    break;
+                case 2:
+                    LabelBalanced.Content = "Selected";
+                    LabelBalanced.Visibility = Visibility.Visible;
+                    LabelHigh.Visibility = Visibility.Hidden;
+                    LabelLow.Visibility = Visibility.Hidden;
+                    break;
+                case 3:
+                    LabelHigh.Content = "Selected";
+                    LabelHigh.Visibility = Visibility.Visible;
+                    LabelLow.Visibility = Visibility.Hidden;
+                    LabelBalanced.Visibility = Visibility.Hidden;
+                    break;
+                default:
+                    LabelBalanced.Content = "Selected";
+                    LabelBalanced.Visibility = Visibility.Visible;
+                    LabelHigh.Visibility = Visibility.Hidden;
+                    LabelLow.Visibility = Visibility.Hidden;
+                    break;
+            }
+        }
+
+        static public void GetChargingStatus()
+        {
+            SelectQuery Sq = new SelectQuery("Win32_Battery");
+            ManagementObjectSearcher objOSDetails = new ManagementObjectSearcher(Sq);
+            ManagementObjectCollection osDetailsCollection = objOSDetails.Get();
+            StringBuilder sb = new StringBuilder();
+
+            Boolean Charging = false;
+
+            foreach (ManagementObject mo in osDetailsCollection)
+            {
+                if ((ushort)mo["BatteryStatus"] == 2)
+                {
+                    Charging = true;
+                    
+                }
+                else
+                {
+                    Charging = false;
+                    
+                }
+            }
+            //MessageBox.Show(Charging.ToString());
+           
+        }
+
+
+        static public int GetCurrentPowerProfile()
+        {
+            ProcessStartInfo ps = new ProcessStartInfo();
+            ps.CreateNoWindow = true;
+            ps.UseShellExecute = false;
+            ps.FileName = "cmd.exe";
+            ps.Arguments = @"/c powercfg -getactivescheme";
+            ps.RedirectStandardOutput = true;
+            var proc = Process.Start(ps);
+
+            string s = proc.StandardOutput.ReadToEnd();
+
+            if (s.Contains(LowGUID))
+            {
+                return 1;
+            }
+            else if (s.Contains(BalancedGUID))
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+
+        }
+
+
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -97,9 +201,9 @@ namespace Switch_Power_profile
                 ps.Arguments = @"/c powercfg -setactive ca85b8be-8830-40df-94f7-86fe770005f1";
                 ps.RedirectStandardOutput = true;
                 var proc = Process.Start(ps);
+                string s = proc.StandardOutput.ReadToEnd();
 
                 LabelHigh.Content = "Selected";
-                string s = proc.StandardOutput.ReadToEnd();
                 LabelHigh.Visibility = Visibility.Visible;
                 LabelLow.Visibility = Visibility.Hidden;
                 LabelBalanced.Visibility = Visibility.Hidden;
@@ -150,5 +254,7 @@ namespace Switch_Power_profile
             task.Start();
             
         }
+
+
     }
 }
