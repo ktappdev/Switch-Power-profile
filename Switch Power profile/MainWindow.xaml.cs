c6428308702c2903f55dfdef6cbf55a2d8b9fced
KTAppDev <kentaylorappdev@gmail.com>
2020-12-14 17:03:36 -0400

    Nice Upgrades, still working on switching profile based on charging status. getting issue with other process owner

--- Switch Power profile/MainWindow.xaml.cs
+++ Switch Power profile/MainWindow.xaml.cs
@@ -54,33 +54,99 @@ namespace Switch_Power_profile
             switch (State)
             {
                 case 1:
-                    LabelLow.Content = "Selected";
-                    LabelLow.Visibility = Visibility.Visible;
-                    LabelBalanced.Visibility = Visibility.Hidden;
-                    LabelHigh.Visibility = Visibility.Hidden;
+                    SetLowProfile();
                     break;
                 case 2:
-                    LabelBalanced.Content = "Selected";
-                    LabelBalanced.Visibility = Visibility.Visible;
-                    LabelHigh.Visibility = Visibility.Hidden;
-                    LabelLow.Visibility = Visibility.Hidden;
+                    SetBalancedProfile();
                     break;
                 case 3:
-                    LabelHigh.Content = "Selected";
-                    LabelHigh.Visibility = Visibility.Visible;
-                    LabelLow.Visibility = Visibility.Hidden;
-                    LabelBalanced.Visibility = Visibility.Hidden;
+                    SetHighProfile();
                     break;
                 default:
-                    LabelBalanced.Content = "Selected";
-                    LabelBalanced.Visibility = Visibility.Visible;
-                    LabelHigh.Visibility = Visibility.Hidden;
-                    LabelLow.Visibility = Visibility.Hidden;
+                    SetBalancedProfile();
                     break;
             }
         }
 
-        static public void GetChargingStatus()
+        public void SetLowProfile()
+        {
+            if (LabelLow.Visibility != Visibility.Visible)
+            {
+                ProcessStartInfo ps = new ProcessStartInfo();
+                ps.CreateNoWindow = true;
+                ps.UseShellExecute = false;
+                ps.FileName = "cmd.exe";
+                ps.Arguments = $@"/c powercfg -setactive {LowGUID}";
+                ps.RedirectStandardOutput = true;
+                var proc = Process.Start(ps);
+
+                //string s = proc.StandardOutput.ReadToEnd();
+
+
+                LabelLow.Content = $"Selected";
+                LabelLow.Visibility = Visibility.Visible;
+                LabelBalanced.Visibility = Visibility.Hidden;
+                LabelHigh.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                LabelLow.Content = "Already selected";
+            }
+        }
+
+
+        public void SetBalancedProfile()
+        {
+            if (LabelBalanced.Visibility != Visibility.Visible)
+            {
+                ProcessStartInfo ps = new ProcessStartInfo();
+                ps.CreateNoWindow = true;
+                ps.UseShellExecute = false;
+                ps.FileName = "cmd.exe";
+                ps.Arguments = $@"/c powercfg -setactive {BalancedGUID}";
+                ps.RedirectStandardOutput = true;
+                var proc = Process.Start(ps);
+
+                //string s = proc.StandardOutput.ReadToEnd();
+
+                LabelBalanced.Content = "Selected";
+                LabelBalanced.Visibility = Visibility.Visible;
+                LabelHigh.Visibility = Visibility.Hidden;
+                LabelLow.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                LabelBalanced.Content = "Already selected";
+            }
+        }
+
+        public void SetHighProfile()
+        {
+            if (LabelHigh.Visibility != Visibility.Visible)
+            {
+
+                ProcessStartInfo ps = new ProcessStartInfo();
+                ps.CreateNoWindow = true;
+                ps.UseShellExecute = false;
+                ps.FileName = "cmd.exe";
+                ps.Arguments = $@"/c powercfg -setactive {HighGUID}";
+                ps.RedirectStandardOutput = true;
+                var proc = Process.Start(ps);
+                //string s = proc.StandardOutput.ReadToEnd();
+
+                LabelHigh.Content = "Selected";
+                LabelHigh.Visibility = Visibility.Visible;
+                LabelLow.Visibility = Visibility.Hidden;
+                LabelBalanced.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                LabelHigh.Content = "Already selected";
+            }
+        }
+
+
+        public void GetChargingStatus()
         {
             SelectQuery Sq = new SelectQuery("Win32_Battery");
             ManagementObjectSearcher objOSDetails = new ManagementObjectSearcher(Sq);
@@ -94,20 +160,24 @@ namespace Switch_Power_profile
                 if ((ushort)mo["BatteryStatus"] == 2)
                 {
                     Charging = true;
+                    if(GetCurrentPowerProfile() != 3)
+                    {
+
+                        SetBalancedProfile();
+                    }
                     
                 }
                 else
                 {
                     Charging = false;
-                    
+                    SetLowProfile();
                 }
             }
-            //MessageBox.Show(Charging.ToString());
            
         }
 
 
-        static public int GetCurrentPowerProfile()
+        public int GetCurrentPowerProfile()
         {
             ProcessStartInfo ps = new ProcessStartInfo();
             ps.CreateNoWindow = true;
@@ -135,91 +205,11 @@ namespace Switch_Power_profile
         }
 
 
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void GetProcessesList()
         {
-            if (LabelLow.Visibility != Visibility.Visible)
-            {
-                ProcessStartInfo ps = new ProcessStartInfo();
-                ps.CreateNoWindow = true;
-                ps.UseShellExecute = false;
-                ps.FileName = "cmd.exe";
-                ps.Arguments = @"/c powercfg -setactive b75672a1-4a7f-4485-9a64-0445bab63964";
-                ps.RedirectStandardOutput = true;
-                var proc = Process.Start(ps);
-
-                string s = proc.StandardOutput.ReadToEnd();
-
-                
-                LabelLow.Content = $"Selected";
-                LabelLow.Visibility = Visibility.Visible;
-                LabelBalanced.Visibility = Visibility.Hidden;
-                LabelHigh.Visibility = Visibility.Hidden;
-            }
-            else
-            {
-                LabelLow.Content = "Already selected";
-            }
-            
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (LabelBalanced.Visibility != Visibility.Visible)
-            {
-                ProcessStartInfo ps = new ProcessStartInfo();
-                ps.CreateNoWindow = true;
-                ps.UseShellExecute = false;
-                ps.FileName = "cmd.exe";
-                ps.Arguments = @"/c powercfg -setactive 381b4222-f694-41f0-9685-ff5bb260df2e";
-                ps.RedirectStandardOutput = true;
-                var proc = Process.Start(ps);
-
-                string s = proc.StandardOutput.ReadToEnd();
-
-                LabelBalanced.Content = "Selected";
-                LabelBalanced.Visibility = Visibility.Visible;
-                LabelHigh.Visibility = Visibility.Hidden;
-                LabelLow.Visibility = Visibility.Hidden;
-            }
-            else
-            {
-                LabelBalanced.Content = "Already selected";
-            }
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            if (LabelHigh.Visibility != Visibility.Visible)
-            {
-
-                ProcessStartInfo ps = new ProcessStartInfo();
-                ps.CreateNoWindow = true;
-                ps.UseShellExecute = false;
-                ps.FileName = "cmd.exe";
-                ps.Arguments = @"/c powercfg -setactive ca85b8be-8830-40df-94f7-86fe770005f1";
-                ps.RedirectStandardOutput = true;
-                var proc = Process.Start(ps);
-                string s = proc.StandardOutput.ReadToEnd();
-
-                LabelHigh.Content = "Selected";
-                LabelHigh.Visibility = Visibility.Visible;
-                LabelLow.Visibility = Visibility.Hidden;
-                LabelBalanced.Visibility = Visibility.Hidden;
-            }
-            else
-            {
-                LabelHigh.Content = "Already selected";
-            }
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-
             Task task = new Task(() =>
             {
-                
+
                 List<string> processlist = new List<string>();
 
                 Process[] processCollection = Process.GetProcesses();
@@ -229,30 +219,52 @@ namespace Switch_Power_profile
                     {
                         processlist.Add(p.ProcessName);
                     }
-
                 }
 
                 this.Dispatcher.Invoke(() =>
                 {
                     ListBoxProcesses.Items.Clear();
                 });
-                    
+
 
                 processlist.Sort();
                 foreach (var process in processlist)
                 {
                     this.Dispatcher.Invoke(() =>
                     {
-                        ListBoxProcesses.Items.Add(process);
+                        ListBoxProcesses.Items.Add(process); // populate a listbox or setup a return
                     });
-                        
+
                 }
                 processlist.Clear();
-                Thread.Sleep(1000);
-                
+                //Thread.Sleep(1000);
+
             });
             task.Start();
+        }
+
+
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            GetProcessesList();
             
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetLowProfile();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SetBalancedProfile();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SetHighProfile();
         }
 
 
